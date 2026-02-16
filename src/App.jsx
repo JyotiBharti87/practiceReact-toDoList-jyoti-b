35f505c91e5b8082097b43363ce5c429342cb31d
jyotibharti87 <joybharti87@gmail.com>
2026-02-15 22:43:21 -0600

    rest of the files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header'; //import header
+import ToDoList from './components/ToDoList';  //import todolist
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
